9427495378e7ca46600b27e67c6e1eb70bd1afec
COZMIKDX <tacocatdx@gmail.com>
2022-02-26 21:02:45 -0600

    Hunter X Hunter reference fixed

--- src/owner.js
+++ src/owner.js
@@ -1,20 +1,23 @@
-const {MessageEmbed} = require("discord.js");
+const Discord = require("discord.js");
+
 
 //Figure out what to do with this. A starter maybe?
-if (incomingMessage.includes("i caught the bomber"))
-{
-  const embed = new MessageEmbed()
-        //.setTitle("You X Beat X Greed X Island!")
-        //.setAuthor()
-        .setColor(0xffffff)
-        //.setDescription("You dirty old man. Here you go anyways")
-        //.setFooter()
-        .setImage("http://i.imgur.com/nIau2PV.jpg")
-        //.setThumbnail()
-        //.setTimestamp()
-        //.setURL()
-        //.addField("")
-        //.addBlankField(true)
+exports.bomber = function(text, message) {
+  if (text.includes("i caught the bomber"))
+  {
+    let embed = new Discord.MessageEmbed()
+          //.setTitle("You X Beat X Greed X Island!")
+          //.setAuthor()
+          .setColor(0xffffff)
+          //.setDescription("You dirty old man. Here you go anyways")
+          //.setFooter()
+          .setImage("http://i.imgur.com/nIau2PV.jpg")
+          //.setThumbnail()
+          //.setTimestamp()
+          //.setURL()
+          //.addField("")
+          //.addBlankField(true)
 
-  message.channel.send({ embeds: [embed] });
+    message.channel.send({ embeds: [embed] });
+  }
 }
