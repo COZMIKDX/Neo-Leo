855171160a742cad3dc893053a218dbeec5f5be4
COZMIKDX <tacocatdx@gmail.com>
2022-03-11 20:27:13 -0600

    updated some syntax.

--- src/prefixCommands.js
+++ src/prefixCommands.js
@@ -1,94 +1,76 @@
-//Not complete. I still need to make 
-
 //const Discord = require("discord.js");
 const {MessageEmbed} = require('discord.js');
 const myUtils = require('./myUtils.js');
 
-
-exports.bday = function(message)
+// Node.js uses CommonJS format. So, it uses module.exports and require() rather than export and import.
+exports.bday = (message) =>
 {
 	let embed = new MessageEmbed()
 		.setTitle("HAPPY BIRTHDAY!!!")
-		//.setAuthor()
 		.setColor(0xf3f72a)
-		//.setDescription("You dirty old man.")
-		//.setFooter()
 		.setImage("https://thumb1.shutterstock.com/display_pic_with_logo/522235/104525213/stock-photo-man-playing-with-party-blowers-104525213.jpg")
-		//.setThumbnail()
-		.setTimestamp()
-		//.setURL()
-		//.addField("")
-		//.addBlankField(true)
+		.setTimestamp();
 
 	message.channel.send({ embeds: [embed] });
-}
+};
 
 
-const friendURLs = ["https://cdn.glitch.com/843b538e-c8b4-4e06-9e17-0de07a61c029%2Fchallenger_leo.png?1544592269001", "https://cdn.glitch.com/843b538e-c8b4-4e06-9e17-0de07a61c029%2FFightersPassDLC.png?1544592242890", "https://cdn.glitch.com/843b538e-c8b4-4e06-9e17-0de07a61c029%2FcaidenAlpha.png?1544592259586", "https://cdn.glitch.com/843b538e-c8b4-4e06-9e17-0de07a61c029%2FnewCaiden.jpg?1543260895298", "https://cdn.glitch.com/843b538e-c8b4-4e06-9e17-0de07a61c029%2FN_O_T_O_W_L_C_I_T_Y.PNG?1544592244642"];
-
-exports.caiden = function(message)
+const caidenImages = [
+	"https://i.imgur.com/Asta7mE.png",
+	"https://i.imgur.com/t6l4Vtw.jpg"
+];
+exports.caiden = (message) =>
 {
-	var index = myUtils.getRandomInt(2,3);
-	var url = friendURLs[index];
+	var index = myUtils.getRandomInt(0, caidenImages.length);
+	var url = caidenImages[index];
 	let embed = new MessageEmbed()
 		.setTitle("He is still hungry")
-		//.setAuthor()
 		.setColor(0x1db207)
-		//.setDescription("You dirty old man.")
-		//.setFooter()
 		.setImage(url)
-		//https://media.discordapp.net/attachments/320339084172066823/397275316894040064/caidenAlpha.png
-		//
-		//.setThumbnail()
-		.setTimestamp()
-		//.setURL()
-		//.addField("")
-		//.addBlankField(true)
+		.setTimestamp();
 
 	message.channel.send({ embeds: [embed] });
-}
+};
 
-exports.leo = function(message)
+const leoImages = [
+	"https://i.imgur.com/HpFkiYt.png",
+	"https://i.imgur.com/qbLXdfb.png",
+	"https://i.imgur.com/mRWnbAA.png"
+];
+exports.leo = (message) =>
 {
-	var index = myUtils.getRandomInt(0,1);
-	var url = friendURLs[index];
+	var index = myUtils.getRandomInt(0, leoImages.length);
+	var url = leoImages[index];
 
 	let embed = new MessageEmbed()
 		.setTitle("The eater of worlds has entered the field!!!")
-		//.setAuthor()
 		.setColor(0x1db207)
 		.setDescription("Lock your refrigerator")
-		//.setFooter()
 		.setImage(url)
-		//.setThumbnail()
-		.setTimestamp()
-		//.setURL()
-		//.addField("")
-		//.addBlankField(true)
+		.setTimestamp();
 
 	message.channel.send({ embeds: [embed] });
 }
 
-exports.marsh = function(message)
+const marshImages = [
+	"https://i.imgur.com/QUelW1I.png",
+	"https://i.imgur.com/3bS0v6U.png",
+	"https://i.imgur.com/8Qnjs9Y.png"
+];
+exports.marsh = (message) =>
 {
-	var index = myUtils.getRandomInt(4,4);
-	var url = friendURLs[index];
+	var index = myUtils.getRandomInt(0, marshImages.length);
+	var url = marshImages[index];
 
 	let embed = new MessageEmbed()
 	.setTitle("What is Marsh? We just don't know.")
-	//.setAuthor()
 	.setColor(0x1db207)
 	.setDescription("Hide your rope!")
-	//.setFooter()
 	.setImage(url)
-	//.setThumbnail()
-	.setTimestamp()
-	//.setURL()
-	//.addField("")
-	//.addBlankField(true)
+	.setTimestamp();
 
 	message.channel.send({ embeds: [embed] });
 }
 
-exports.money = function(message){}
+exports.money = (message) => {}
 
