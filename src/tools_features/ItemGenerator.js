03178d516dbc7738de307be2012d1129f6404431
COZMIKDX <tacocatdx@gmail.com>
2023-03-03 18:10:32 -0600

    adjusted generator to take boiler plate(?) text in the sentence

--- src/tools_features/ItemGenerator.js
+++ src/tools_features/ItemGenerator.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const myUtils = require('../myUtils.js');
-let parsedOb;
+// let parsedOb;
+// let sentenceConst = {};
 
 // Right now, a new generator will be created each time the user calls a generation command.
 // This means the json file will be read and parsed each time.
@@ -8,16 +9,17 @@ let parsedOb;
 //  is completed since we like to spam it.
 //  If keep that in a static variable, it'll always be taking up memory.
 class ItemGenerator {
-    constructor(filepath) {
+    constructor(filepath, sentenceInfo = {}) {
         let data = fs.readFileSync(filepath);
-        parsedOb = JSON.parse(data);
+        this.parsedOb = JSON.parse(data);
+        this.sentenceInfo = sentenceInfo;
     }
 
     generate() {
         let output = "";
-        for (let field in parsedOb) {
-            let item = parsedOb[field][myUtils.getRandomInt(0, parsedOb[field].length - 1)];
-            if (field == "Modifiers") {
+        for (let field in this.parsedOb) {
+            let item = this.parsedOb[field][myUtils.getRandomInt(0, this.parsedOb[field].length - 1)];
+            if (field == "modifier") {
                 let rand = Math.random();
                 if (rand < .2) { // add the modifier if we hit the 20% chance
                     if (item.startsWith(',')) {
@@ -30,6 +32,11 @@ class ItemGenerator {
             }
             else { // add any non-modifier item to the string.
                 output = output + " " + item;
+            }
+
+            // Add in boiler plate text such as "potion of", if it exists.
+            if (field == this.sentenceInfo["field"]) {
+                output = output + " " + this.sentenceInfo["text"];
             }
         }
 
--- src/tools_features/potionGen.js
+++ src/tools_features/potionGen.js
@@ -7,7 +7,8 @@ exports.potionGen = function()
   // I think the path is relative to the directory you launched the bot from.
   // I'll make it absolute in case I move the ItemGenerator file later.
   let absolutePath = path.resolve("./tools_features/potionGen.json");
-  let potionGenerator = new generator(absolutePath);
+  let sentenceInfo = {field: "color", text: "potion of"};
+  let potionGenerator = new generator(absolutePath, sentenceInfo);
   return potionGenerator.generate();
 }
 
