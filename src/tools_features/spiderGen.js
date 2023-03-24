a25f3618c46bf82699707401d26ef584a551a593
COZMIKDX <tacocatdx@gmail.com>
2023-03-23 23:32:07 -0500

    removed comment

--- src/tools_features/spiderGen.js
+++ src/tools_features/spiderGen.js
@@ -4,8 +4,6 @@ const generator = require('./ItemGenerator.js');
 
 exports.spiderNameGen = function()
 {
-  // I think the path is relative to the directory you launched the bot from.
-  // I'll make it absolute in case I move the ItemGenerator file later.
   let absolutePath = path.resolve("./tools_features/spodeGen.json");
   let spodeGenerator = new generator(absolutePath);
   return spodeGenerator.generate();
